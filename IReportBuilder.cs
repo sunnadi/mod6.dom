953fab9b383ca9fd1d195dfa88538a732add8939
Suneliya <149096669+sunnadi@users.noreply.github.com>
2024-10-19 18:21:33 +0500

    Add files via upload

---
+++ IReportBuilder.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+public class Report
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public string Footer { get; set; }
+
+    public void Show()
+    {
+        Console.WriteLine(Title);
+        Console.WriteLine(Body);
+        Console.WriteLine(Footer);
+    }
+}
+
+public interface IReportBuilder
+{
+    void SetTitle(string title);
+    void SetBody(string content);
+    void SetFooter(string footer);
+    Report GetReport();
+}
+
+public class TextReportBuilder : IReportBuilder
+{
+    private Report _report;
+
+    public TextReportBuilder()
+    {
+        _report = new Report();
+    }
+
+    public void SetTitle(string title)
+    {
+        _report.Title = "Текстовый отчет: " + title;
+    }
+
+    public void SetBody(string content)
+    {
+        _report.Body = content;
+    }
+
+    public void SetFooter(string footer)
+    {
+        _report.Footer = footer;
+    }
+
+    public Report GetReport()
+    {
+        return _report;
+    }
+}
+
+public class HtmlReportBuilder : IReportBuilder
+{
+    private Report _report;
+
+    public HtmlReportBuilder()
+    {
+        _report = new Report();
+    }
+
+    public void SetTitle(string title)
+    {
+        _report.Title = $"<h1>{title}</h1>";
+    }
+
+    public void SetBody(string content)
+    {
+        _report.Body = $"<p>{content}</p>";
+    }
+
+    public void SetFooter(string footer)
+    {
+        _report.Footer = $"<footer>{footer}</footer>";
+    }
+
+    public Report GetReport()
+    {
+        return _report;
+    }
+}
+
+public class ReportDirector
+{
+    public void BuildReport(IReportBuilder builder)
+    {
+        builder.SetTitle("Ежегодный отчет");
+        builder.SetBody("Тело отчета(содержимое) что то что то");
+        builder.SetFooter("Подвал отчета(сслыки, литература и тп.)");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var director = new ReportDirector();
+
+        IReportBuilder textBuilder = new TextReportBuilder();
+        director.BuildReport(textBuilder);
+        var textReport = textBuilder.GetReport();
+        textReport.Show();
+
+        Console.WriteLine();
+
+        IReportBuilder htmlBuilder = new HtmlReportBuilder();
+        director.BuildReport(htmlBuilder);
+        var htmlReport = htmlBuilder.GetReport();
+        htmlReport.Show();
+    }
+}
---
+++ Prototypyintmag.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Product : ICloneable
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, decimal price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public object Clone()
+    {
+        return new Product(Name, Price, Quantity);
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, Price: {Price}, Quantity: {Quantity}";
+    }
+}
+
+public class Discount : ICloneable
+{
+    public string Name { get; set; }
+    public decimal Amount { get; set; }
+
+    public Discount(string name, decimal amount)
+    {
+        Name = name;
+        Amount = amount;
+    }
+
+    public object Clone()
+    {
+        return new Discount(Name, Amount);
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, Amount: {Amount}";
+    }
+}
+
+public class Order : ICloneable
+{
+    public List<Product> Products { get; set; }
+    public decimal DeliveryCost { get; set; }
+    public Discount AppliedDiscount { get; set; }
+    public string PaymentMethod { get; set; }
+
+    public Order(List<Product> products, decimal deliveryCost, Discount discount, string paymentMethod)
+    {
+        Products = products;
+        DeliveryCost = deliveryCost;
+        AppliedDiscount = discount;
+        PaymentMethod = paymentMethod;
+    }
+
+    
+    public object Clone()
+    {
+        var clonedProducts = new List<Product>();
+        foreach (var product in Products)
+        {
+            clonedProducts.Add((Product)product.Clone());
+        }
+
+        return new Order(clonedProducts, DeliveryCost, (Discount)AppliedDiscount.Clone(), PaymentMethod);
+    }
+
+    public override string ToString()
+    {
+        string productDetails = string.Join("\n", Products);
+        return $"Products:\n{productDetails}\nDelivery Cost: {DeliveryCost}\nDiscount: {AppliedDiscount}\nPayment Method: {PaymentMethod}";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var product1 = new Product("Книга: Война и Мир", 500.00m, 1);
+        var product2 = new Product("Тетрадь 96 листов", 40.00m, 5);
+
+        var discount = new Discount("Осенняя распродажа", 50.00m);
+
+        var originalOrder = new Order(new List<Product> { product1, product2 }, 150.00m, discount, "Наличный расчет");
+
+        Console.WriteLine("Оригинальный заказ:");
+        Console.WriteLine(originalOrder);
+
+        var clonedOrder = (Order)originalOrder.Clone();
+
+        clonedOrder.Products[0].Name = "Книга: Преступление и Наказание";
+        clonedOrder.PaymentMethod = "Банковская карта";
+
+        Console.WriteLine("\nКлонированный заказ (с изменениями):");
+        Console.WriteLine(clonedOrder);
+
+        Console.WriteLine("\nОригинальный заказ (после клонирования):");
+        Console.WriteLine(originalOrder); 
+    }
+}
+
